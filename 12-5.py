72597d05493866c34b78438b969681de42febf54
Franco660 <83373195+Franco660@users.noreply.github.com>
2021-05-13 17:30:41 -0300

    Add files via upload

---
+++ 12-5.py
@@ -0,0 +1,40 @@
+#EJERCICIO 3
+class Empleado:
+    def __init__(self, nombre, sueldo):
+        self.nombre = input("Ingrese el nombre del empleado")
+        self.sueldo = int(input("Ingrese el sueldo del empleado"))
+
+
+empleadoDatos = Empleado("", 0)
+
+if __name__ == "__main__":
+    print("El empleado se llama", empleadoDatos.nombre)
+    print("Su sueldo es de", empleadoDatos.sueldo, "pesos")
+    if empleadoDatos.sueldo > 3000:
+        print("Por lo tanto, debera pagar impuestos")
+    else:
+        print("No debe pagar impuestos")
+"""
+#EJERCICIO 4
+
+
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+
+class EmpleadoHered(Persona):
+    def __init__(self, nombre, edad, sueldo):
+        super().__init__(nombre, edad)
+        self.sueldo = sueldo
+
+
+if __name__ == "__main__":
+    datosPersona = Persona(input("Ingrese su nombre "), input("Ingrese su edad "))
+    datosEmpleado = EmpleadoHered(datosPersona.nombre, datosPersona.edad, input("Ingrese su sueldo "))
+    print("El nombre ingresado es:", datosPersona.nombre)
+    print("La edad ingresada es:", datosPersona.edad)
+    print("El sueldo ingresado es:", datosEmpleado.sueldo)
+
+"""
